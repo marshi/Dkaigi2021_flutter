e9fb4d405e01263c639208d573142b9017235de9
marshi <mashi3648@gmail.como>
2021-04-30 23:21:29 +0900

    無理やりスクロール

--- lib/ui/dhome/dhome_page.dart
+++ lib/ui/dhome/dhome_page.dart
@@ -16,6 +16,8 @@ class _DHomePageState extends State<DHomePage> {
     const Tab(text: "Podcast"),
   ];
 
+  final key = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,25 +30,56 @@ class _DHomePageState extends State<DHomePage> {
               tabs: _tab,
             ),
           ),
-          backLayer: BackdropNavigationBackLayer(items: [
+          backLayer: BackdropNavigationBackLayer(key: key, items: [
             Text("TabBar"),
             Text("TabBar2"),
           ]),
           stickyFrontLayer: true,
           frontLayer: ScrollConfiguration(
             behavior: MyBehavior(),
-            child: TabBarView(
-              children: [
-                HomeTab(
-                  icon: Icons.directions_car,
-                ),
-                TabPage(icon: Icons.directions_bike),
-                TabPage(icon: Icons.directions_bike),
-                TabPage(icon: Icons.directions_bike),
-              ],
-            ),
+            child: NewWidget(backLayerKey: key),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  final GlobalKey? backLayerKey;
+  double y = 1.0;
+
+  NewWidget({
+    Key? key,
+    this.backLayerKey,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        final height = (backLayerKey?.currentContext?.size?.height);
+        final dy = details.delta.dy;
+        y -= dy;
+        y = y.clamp(0.0, height!);
+        var backdrop = Backdrop.of(context);
+        print("$y $dy $height");
+        final animateTo = (y - dy) / height;
+        final ani = animateTo.clamp(0.0, 1.0);
+        print("(${y} - $dy) / $height = ${ani}");
+        backdrop.widget.backLayer.key;
+        backdrop.animationController
+            .animateTo(ani, duration: Duration(seconds: 1));
+      },
+      child: TabBarView(
+        children: [
+          HomeTab(
+            icon: Icons.directions_car,
+          ),
+          TabPage(icon: Icons.directions_bike),
+          TabPage(icon: Icons.directions_bike),
+          TabPage(icon: Icons.directions_bike),
+        ],
       ),
     );
   }
